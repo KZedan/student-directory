87c1716983469298e24d6e7659e7c0a98187cd44
Kareem <kareem335@yahoo.co.uk>
2019-06-10 21:29:31 +0100

    excercise 7

--- directory.rb
+++ directory.rb
@@ -1,25 +1,28 @@
 
-def input_students
+def input_students(cohort = :november)
   puts "Please enter the names of the students followed by their"
   puts "- Hobbies".center(50)
   puts "- Country of birth".center(50)
   puts "- Height".center(50)
+  puts "- Cohort".center(50)
   puts "To finish, just hit return twice".center(50)
 
   students = []
 
   name = gets.chomp
-  hobbies = gets.chomp
-  country_of_birth = gets.chomp
-  height = gets.chomp
 
   while !name.empty? do
-    students << {name: name, hobbies: hobbies, country: country_of_birth, height: height, cohort: :november}
-    puts "Now we have #{students.count} students"
-    name = gets.chomp
     hobbies = gets.chomp
     country_of_birth = gets.chomp
     height = gets.chomp
+    cohort = gets.chomp
+    if cohort != "november"
+      puts "invalid cohort!"
+      cohort = "november"
+    end
+    students << {name: name, hobbies: hobbies, country: country_of_birth, height: height, cohort: cohort.to_sym}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
   end
   students
 end
