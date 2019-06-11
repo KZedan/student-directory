7b89d1e54702ce398651bd86562b470934da755c
Kareem <kareem335@yahoo.co.uk>
2019-06-11 15:02:38 +0100

    added interactive menu

--- directory.rb
+++ directory.rb
@@ -1,11 +1,33 @@
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    selection = gets.chomp
+
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
 
 def input_students
-  puts "Please enter the names of the students followed by their"
-  puts "- Hobbies".center(50)
-  puts "- Country of birth".center(50)
-  puts "- Height".center(50)
-  puts "- Cohort".center(50)
-  puts "To finish, just hit return twice".center(50)
+  puts "Please enter the name of the students followed by their"
+  puts "- Hobbies"
+  puts "- Country of birth"
+  puts "- Height"
+  puts "- Cohort"
+  puts "To finish, just hit return twice"
 
   cohorts = ["january", "febuary", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
   students = []
@@ -48,10 +70,7 @@ def print(students)
   end
 
   students.each_with_index do |student, index|
-    while student[:name][0] == "L" && student[:name].length < 12
-      puts "#{index+1} #{student[:name]} #{student[:hobbies]} #{student[:country]} #{student[:height]} (#{student[:cohort]} cohort)"
-      break
-    end
+    puts "#{index+1} #{student[:name]} #{student[:hobbies]} #{student[:country]} #{student[:height]} (#{student[:cohort]} cohort)"
   end
 end
 
@@ -63,7 +82,4 @@ def print_footer(names)
   end
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+interactive_menu
