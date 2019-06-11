759257c696d7f22537bd27f27e3bea470ceff202
Kareem <kareem335@yahoo.co.uk>
2019-06-11 12:09:44 +0100

    added if one student and tidied things up a bit

--- directory.rb
+++ directory.rb
@@ -7,21 +7,31 @@ def input_students
   puts "- Cohort".center(50)
   puts "To finish, just hit return twice".center(50)
 
-  students = []
   cohorts = ["january", "febuary", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
+  students = []
   name = gets.chomp
+  count = 0
 
   while !name.empty? do
     hobbies = gets.chomp
     country_of_birth = gets.chomp
     height = gets.chomp
     cohort = gets.chomp
+    count += 1
+
     if cohorts.index(cohort.downcase) == nil
       puts "Invalid cohort! Cohort set to november by default."
       cohort = "november"
     end
+
     students << {name: name, hobbies: hobbies, country: country_of_birth, height: height, cohort: cohort.to_sym}
-    puts "Now we have #{students.count} students"
+
+    if count == 1
+      puts "Now we have #{students.count} student"
+    else
+      puts "Now we have #{students.count} students"
+    end
+
     name = gets.chomp
   end
   students
@@ -36,6 +46,7 @@ def print(students)
   students = students.sort_by do |student|
     student[:cohort]
   end
+
   students.each_with_index do |student, index|
     while student[:name][0] == "L" && student[:name].length < 12
       puts "#{index+1} #{student[:name]} #{student[:hobbies]} #{student[:country]} #{student[:height]} (#{student[:cohort]} cohort)"
@@ -45,7 +56,11 @@ def print(students)
 end
 
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+  if names.count == 1
+    puts "Overall, we have #{names.count} great student"
+  else
+    puts "Overall, we have #{names.count} great students"
+  end
 end
 
 students = input_students
